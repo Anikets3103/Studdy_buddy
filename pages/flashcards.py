9d61f66be7d18458c9dbb0341d2a685276bb0214
Anikets3103 <158808975+Anikets3103@users.noreply.github.com>
2025-09-27 19:47:38 +0530

    Update flashcards.py

--- pages/flashcards.py
+++ pages/flashcards.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 API_KEY = os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=API_KEY)
-model = genai.GenerativeModel("gemini-1.5-flash") 
+model = genai.GenerativeModel("gemini-2.5-flash") 
 st.markdown(
     """
     <style>
